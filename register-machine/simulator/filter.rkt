e669ac61950ef5f183226a9b898d2096dd8096a0
reckonbytes <22992867+reckonbytes@users.noreply.github.com>
2024-08-29 00:26:11 +0000

    Create filter.rkt

---
+++ register-machine/simulator/filter.rkt
@@ -0,0 +1,8 @@
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(#%provide filter)
