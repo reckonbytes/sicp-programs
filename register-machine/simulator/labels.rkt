f2d14709588cb8648164eeb3c4c6cbb366ddf266
reckonbytes <22992867+reckonbytes@users.noreply.github.com>
2024-09-18 00:01:43 +0000

    Update labels.rkt for adding breakpoint to simulator
    Added restored-regs and max-breakpoint to label entry.
    Changed table interface to dispatch.

--- register-machine/simulator/labels.rkt
+++ register-machine/simulator/labels.rkt
@@ -1,58 +1,87 @@
 #lang sicp
 
-(#%require "table.rkt"
-           "controller-syntax.rkt")
+(#%require "controller-syntax.rkt")
 
-(define (make-label-entry label-name inst-seq label-regs)
-  (list label-name inst-seq label-regs))
+(define (make-label-entry label-name inst-seq label-regs max-breakpoint)
+  (list label-name inst-seq label-regs max-breakpoint))
+
+(define (get-label label-entry)
+  (car label-entry))
+
+(define (get-inst-seq label-entry)
+  (cadr label-entry))
+
+(define (get-label-regs label-entry)
+  (caddr label-entry))
+
+(define (get-max-breakpoint label-entry)
+  (cadddr label-entry))
+
+(define (extract-label-regs seq)
+  (cond ((null? seq) (list '() seq))
+        
+        ((restored-regs-exp? (car seq))
+         (list (restored-reg-names (car seq))
+               (cdr seq)))
+
+        (else (list '() seq))))
+
+(define (find-max-breakpoint label-insts)
+  (if (or (null? label-insts)
+          (label? (car label-insts)))
+      0
+      (+ 1 (find-max-breakpoint (cdr label-insts)))))
+
+(define (extract-labels ctrl-seq inst-seq)
+  (if (null? ctrl-seq)
+      '()
+      (let ((top (car ctrl-seq)))
+        (cond ((label? top)
+               (if (memq top (cdr ctrl-seq))
+                   (error "Label repeated -- EXTRACT-LABELS" top))
+               (let* ((data (extract-label-regs (cdr ctrl-seq)))
+                      (restored-regs (car data))
+                      (rest-seq (cadr data)))
+            
+                 (cons (make-label-entry
+                        top inst-seq restored-regs (find-max-breakpoint (cdr inst-seq)))
+                       (extract-labels rest-seq (cdr inst-seq)))))
+
+              ((restored-regs-exp? top)
+               (error "Restored-regs instruction must follow a label -- EXTRACT-LABELS"
+                      top))
+
+              (else
+               (extract-labels (cdr ctrl-seq) (cdr inst-seq)))))))
 
 ;the labels are gathered in assoc-list by extract-labels
-;labels-table gives table interface to the labels
-(define (make-labels-table labels-assoc)
-  (let ((lb-tb (make-table)))
-
-    (for-each (lambda (entry)
-                ((lb-tb 'insert!) (car entry) (cdr entry)))
-              labels-assoc)
-
-    (lambda (msg)
-      (cond ((eq? msg 'lookup-label)
-             (lambda (label)
-               (car ((lb-tb 'lookup) label))))
-
-            ((eq? msg 'lookup-label-regs)
-             (lambda (label)
-               (cadr ((lb-tb 'lookup) label))))
-
-            (else (error "Unknown msg -- LABELS TABLE" msg))))))
-
-
-(define (get-labels-table inst-seq inst-text)
-  (make-labels-table (extract-labels inst-seq '() inst-text)))
-
-
-(define (extract-labels inst-seq label-entries inst-text)
-  (if (null? inst-text)
-      label-entries
-      
-      (let ((next-inst (car inst-text)))
-        (if (symbol? next-inst)
-            (if (assoc next-inst label-entries)
-                (error "Label repeated -- ASSEMBLE" next-inst)
-
-                (let* ((label-1st-inst (if (null? (cdr inst-text))
-                                           '()
-                                           (cadr inst-text)))
-                       (regs-saved (restored-reg-names label-1st-inst)))
-                  
-                  (extract-labels
-                   inst-seq
-                   (cons (make-label-entry next-inst inst-seq regs-saved)
-                         label-entries)
-                   (if (restored-regs-exp? label-1st-inst)
-                       (cddr inst-text)
-                       (cdr inst-text)))))
+(define (dispatch-labels labels-assoc)
+  
+  (define (get-label-entry label)
+    (let ((entry (assoc label labels-assoc)))
+      (if entry
+          entry
+          (error "Label entry not found -- LABEL-LOOKUP." label))))
+  
+  (lambda (msg)
+    (cond ((eq? msg 'get-all-labels)
+           (map get-label labels-assoc))
             
-            (extract-labels (cdr inst-seq) label-entries (cdr inst-text))))))
+          ((eq? msg 'lookup)
+           (lambda (label)
+             (get-inst-seq (get-label-entry label))))
 
-(#%provide get-labels-table)
+          ((eq? msg 'lookup-regs)
+           (lambda (label)
+             (get-label-regs (get-label-entry label))))
+
+          ((eq? msg 'max-breakpoint)
+           (lambda (label)
+             (get-max-breakpoint (get-label-entry label))))
+
+          (else (error "Unknown msg -- LABELS TABLE" msg)))))
+
+(define (get-labels-dispatch ctrl-seq inst-seq)
+  (dispatch-labels (extract-labels ctrl-seq inst-seq)))
+
+(#%provide get-labels-dispatch)
