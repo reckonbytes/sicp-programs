896bb981f78b32fb3e9bcf24238357066133c2de
reckonbytes <22992867+reckonbytes@users.noreply.github.com>
2024-10-02 23:16:39 +0000

    Update labels.rkt - removed table interface, assoc-list is used
    The assoc-list generated by extract-labels is passed to the machine.

--- register-machine/simulator/labels.rkt
+++ register-machine/simulator/labels.rkt
@@ -3,19 +3,12 @@
 (#%require "controller-syntax.rkt")
 
 (define (make-label-entry label-name inst-seq label-regs max-breakpoint)
-  (list label-name inst-seq label-regs max-breakpoint))
-
-(define (get-label label-entry)
-  (car label-entry))
-
-(define (get-inst-seq label-entry)
-  (cadr label-entry))
-
-(define (get-label-regs label-entry)
-  (caddr label-entry))
-
-(define (get-max-breakpoint label-entry)
-  (cadddr label-entry))
+  (cons label-name
+        (lambda (key)
+          (cond ((eq? key 'inst-seq) inst-seq)
+                ((eq? key 'label-regs) label-regs)
+                ((eq? key 'max-breakpoint) max-breakpoint)
+                (else (error "Unknown key -- LABEL-ENTRY" key))))))
 
 (define (extract-label-regs seq)
   (cond ((null? seq) (list '() seq))
@@ -54,34 +47,4 @@
               (else
                (extract-labels (cdr ctrl-seq) (cdr inst-seq)))))))
 
-;the labels are gathered in assoc-list by extract-labels
-(define (dispatch-labels labels-assoc)
-  
-  (define (get-label-entry label)
-    (let ((entry (assoc label labels-assoc)))
-      (if entry
-          entry
-          (error "Label entry not found -- LABEL-LOOKUP." label))))
-  
-  (lambda (msg)
-    (cond ((eq? msg 'get-all-labels)
-           (map get-label labels-assoc))
-            
-          ((eq? msg 'lookup)
-           (lambda (label)
-             (get-inst-seq (get-label-entry label))))
-
-          ((eq? msg 'lookup-regs)
-           (lambda (label)
-             (get-label-regs (get-label-entry label))))
-
-          ((eq? msg 'max-breakpoint)
-           (lambda (label)
-             (get-max-breakpoint (get-label-entry label))))
-
-          (else (error "Unknown msg -- LABELS TABLE" msg)))))
-
-(define (get-labels-dispatch ctrl-seq inst-seq)
-  (dispatch-labels (extract-labels ctrl-seq inst-seq)))
-
-(#%provide get-labels-dispatch)
+(#%provide extract-labels)
